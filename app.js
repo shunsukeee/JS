6b71500901d9c0abda3622b11db3f388cd6aeeec
shunsuke <shunsuke.nakayama36@gmail.com>
2020-04-30 14:21:36 +0900

    add2

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-var str1 = 'Hello';
-var str2 = 'World!!aaaa';
-alert(str1 + str2);
+var i;
+var num = 0;
+ 
+for(i = 1; i < 18; i++){
+    num = num + i;
+}
+ 
+alert('1から17まで足し算した結果は' + num + 'です');
